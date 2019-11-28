23b97a5c496d70193024e2766da9b9c2c641ca15
TheMisfit68 <janverrept@me.com>
2019-11-28 22:27:03 +0100

    changed to reading discrete inputs in test

--- Sources/ModbusDriver/ModbusDriver.swift
+++ Sources/ModbusDriver/ModbusDriver.swift
@@ -5,7 +5,7 @@ public class ModbusDriver{
     public func test()->[UInt8]{
         
         let mb = modbus_new_tcp("127.0.0.1", 1502)
-        let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 16)
+        let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 2)
         modbus_connect(mb)
         
         /* Read 5 registers from the address 0 */
@@ -15,7 +15,7 @@ public class ModbusDriver{
         
         var resultArr:[UInt8] = []
         
-        for n in 0...15 {
+        for n in 0...1 {
             resultArr[n] = result[n]
         }
         

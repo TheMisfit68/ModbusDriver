22aff7fada933d86ad5b8b1e83874828f8c96806
TheMisfit68 <janverrept@me.com>
2020-06-24 12:07:39 +0200

    moved to SPM 5.3 and addes resource/artwork

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,13 +13,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/TheMisfit68/ClibModbus.git", .branch("master")),
+        .package(name: "ClibModbus", url: "https://github.com/TheMisfit68/ClibModbus.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ModbusDriver",
-            dependencies: ["ClibModbus"]),
+            dependencies: ["ClibModbus"],
+            exclude:["Artwork"]
+        ),
     ]
 )

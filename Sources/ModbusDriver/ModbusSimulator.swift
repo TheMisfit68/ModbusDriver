1c12475d3ede0cd1f513b8aedb133ca76f5f0932
TheMisfit68 <janverrept@me.com>
2021-02-18 01:58:08 +0100

    Made connection more stable

--- Sources/ModbusDriver/ModbusSimulator.swift
+++ Sources/ModbusDriver/ModbusSimulator.swift
@@ -18,69 +18,36 @@ open class ModbusSimulator: ModbusDriver{
         super.init(ipAddress:ipAddress, port:port)
     }
     
-    deinit {
-        disConnect()
-    }
-    
-    public override func readAllInputs(){
-        
-        switch connectionState{
-        case .disconnecting:
-            disConnect()
-        case .disconnected:
-            connectionState = .connecting
-        case .connecting:
-            print("⛓ connecting @\(ipAddress)")
-            connect()
-        case .connected:
-            print("✅ reading simulated inputs @\(ipAddress)")
-            addressPageSimulatorInputs = 0
-            for modbusModule in modbusModules{
-                let pageStart = addressPageSimulatorInputs*addressPageLengthPerModule
-                
-                let readResult = modbusModule.readAllInputs(connection: modbusConnection, pageStart:pageStart)
-				guard readResult == .noError else{
-                    connectionState = .error
-                    print("❌ error reading simulated inputs @\(ipAddress), module \(modbusModule.rackNumber).\(modbusModule.slotNumber)")
-                    break
-                }
-                addressPageSimulatorInputs += 1
-            }
-        case .error:
-            break // Just wait for automatic retry
-        }
-    }
-    
-    public override func writeAllOutputs(){
-        
-        switch connectionState{
-        case .disconnecting:
-            disConnect()
-        case .disconnected:
-            connectionState = .connecting
-        case .connecting:
-            print("⛓ connecting @\(ipAddress)")
-            connect()
-        case .connected:
-            print("✅ writing simulated outputs @\(ipAddress)")
-            addressPageSimulatorOutputs = 0            
-            for modbusModule in modbusModules{
-                let pageStart = addressPageSimulatorOutputs*addressPageLengthPerModule
+	override func readInputModules() {
+		addressPageSimulatorInputs = 0
+		for modbusModule in modbusModules{
+			let pageStart = addressPageSimulatorInputs*addressPageLengthPerModule
+			
+			let readResult = modbusModule.readAllInputs(connection: modbusConnection, pageStart:pageStart)
+			guard readResult == .noError else{
+				connectionState = .disconnectingWith(targetState: .error)
+				print("❌ error reading simulated inputs @\(ipAddress), module \(modbusModule.rackNumber).\(modbusModule.slotNumber)")
+				break
+			}
+			addressPageSimulatorInputs += 1
+		}
+	}
+	
+	override func writeOutputModules() {
+		addressPageSimulatorOutputs = 0
+		for modbusModule in modbusModules{
+			let pageStart = addressPageSimulatorOutputs*addressPageLengthPerModule
 
-                let writeResult = modbusModule.writeAllOutputs(connection: modbusConnection, addressPage:pageStart)
-				guard writeResult == .noError else{
-                    connectionState = .error
-                    print("❌ error writing simulated inputs @\(ipAddress), module \(modbusModule.rackNumber).\(modbusModule.slotNumber)")
-                    break
-                }
-                addressPageSimulatorOutputs += 1
-            }
-            
-        case .error:
-            break // Just wait for automatic retry
-        }
-    }
-    
+			let writeResult = modbusModule.writeAllOutputs(connection: modbusConnection, addressPage:pageStart)
+			guard writeResult == .noError else{
+				connectionState = .disconnectingWith(targetState: .error)
+				print("❌ error writing simulated inputs @\(ipAddress), module \(modbusModule.rackNumber).\(modbusModule.slotNumber)")
+				break
+			}
+			addressPageSimulatorOutputs += 1
+		}
+	}
+	
 }
 
 

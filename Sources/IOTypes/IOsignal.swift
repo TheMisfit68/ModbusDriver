f6463cce4046201c5b106118378a41a175839b7b
Jan Verrept <janverrept@me.com>
2021-05-26 17:25:44 +0200

    Made logical values of IO-signals into computed property

--- Sources/IOTypes/IOsignal.swift
+++ Sources/IOTypes/IOsignal.swift
@@ -28,10 +28,19 @@ open class IOSignal{
 
 open class AnalogInputSignal:IOSignal{
 	
+	public var ioValue:UInt16?
+	public var ioRange:ClosedRange<UInt16> = 0...UInt16.max
 	var scale:ClosedRange<Float> = 0.0...100.0
 	var unit:String = ""
-	
-	public var logicalValue:Float?
+	public var scaledValue:Float?{
+		guard ioValue != nil else{ return nil}
+		
+		let ioSpan:Float = Float(ioRange.upperBound-ioRange.lowerBound)
+		let percentage = Float(ioValue!-ioRange.lowerBound)/ioSpan
+		
+		let scaleSpan:Float = (scale.upperBound-scale.lowerBound)
+		return scale.lowerBound+(percentage*scaleSpan)
+	}
 	
 	func setScale(_ scale:ClosedRange<Float>, unit:String){
 		self.scale = scale
@@ -42,56 +51,40 @@ open class AnalogInputSignal:IOSignal{
 		super.init(channelType: IOType.analogIn, channelNumber: channelNumber)
 	}
 	
-	public var ioValue:UInt16?{
-		didSet{
-			guard ioValue != nil else{ logicalValue = nil; return }
-			
-			let minScale = scale.lowerBound
-			let range = (scale.upperBound-scale.lowerBound)
-			let ioPercentage = Float(ioValue!)/Float(UInt16.max)
-			logicalValue = minScale+(ioPercentage*range)
-			
-		}
-	}
+	
 }
 
 open class AnalogOutputSignal:IOSignal{
 	
+	public var scaledValue:Float = 0
 	public var scale:ClosedRange<Float> = 0.0...100.0
 	public var unit:String = "%"
 	public var ioRange:ClosedRange<UInt16> = 0...UInt16.max
-	
-	public var scaledValue:Float = 0{
-		didSet{
+	public var ioValue:UInt16{
 			
 			let scaleSpan:Float = (scale.upperBound-scale.lowerBound)
 			let percentage:Float = (scaledValue-scale.lowerBound)/scaleSpan
 			
 			let ioSpan:Float = Float(ioRange.upperBound-ioRange.lowerBound)
-			ioValue = ioRange.lowerBound+UInt16(percentage*ioSpan)
+			return ioRange.lowerBound+UInt16(percentage*ioSpan)
 			
-		}
 	}
-	public var scaledFeedBackValue:Float?
+
+	public var ioFeedbackValue:UInt16?
+	public var scaledFeedBackValue:Float?{
+		guard ioFeedbackValue != nil else{ return nil}
+		
+		let ioSpan:Float = Float(ioRange.upperBound-ioRange.lowerBound)
+		let percentage:Float = Float(ioFeedbackValue!-ioRange.lowerBound)/ioSpan
+		
+		let scaleSpan:Float = (scale.upperBound-scale.lowerBound)
+		return scale.lowerBound+(percentage*scaleSpan)
+	}
 	
 	public init(channelNumber:Int) {
 		super.init(channelType: IOType.analogOut, channelNumber: channelNumber)
 	}
 	
-	public var ioValue:UInt16 = 0
-	
-	public var ioFeedbackValue:UInt16?{
-		didSet{
-			guard ioFeedbackValue != nil else{ scaledFeedBackValue = nil; return }
-			
-			let ioSpan:Float = Float(ioRange.upperBound-ioRange.lowerBound)
-			let percentage:Float = Float(ioFeedbackValue!-ioRange.lowerBound)/ioSpan
-			
-			let scaleSpan:Float = (scale.upperBound-scale.lowerBound)
-
-			scaledFeedBackValue = scale.lowerBound+(percentage*scaleSpan)
-		}
-	}
 }
 
 open class DigitalInputSignal:IOSignal{
@@ -101,22 +94,19 @@ open class DigitalInputSignal:IOSignal{
 		case inverse
 	}
 	
+	public var ioValue:Bool?
 	public var inputLogic: digitalInputLogic = .straight
-	
-	public var logicalValue:Bool?
+	public var logicalValue:Bool?{
+		guard ioValue != nil else{ return  nil}
+		return (inputLogic == .inverse) ? !ioValue! : ioValue!
+	}
 	
 	public init(channelNumber:Int, logic:digitalInputLogic = .straight) {
 		super.init(channelType: IOType.digitalIn, channelNumber: channelNumber)
 		inputLogic = logic
 	}
 	
-	public var ioValue:Bool?{
-		didSet{
-			guard ioValue != nil else{ logicalValue = nil; return }
-			
-			logicalValue = (inputLogic == .inverse) ? !ioValue! : ioValue!
-		}
-	}
+	
 }
 
 open class DigitalOutputSignal:IOSignal{
@@ -125,30 +115,26 @@ open class DigitalOutputSignal:IOSignal{
 		case straight
 		case inverse
 	}
-	
+		
+	public var logicalValue:Bool = false
 	public var outputLogic: digitalOutputLogic = .straight
-	
-	public var logicalValue:Bool = false{
-		didSet{
-			ioValue = (outputLogic == .inverse) ? !logicalValue : logicalValue
-		}
+	public var ioValue:Bool{
+		return (outputLogic == .inverse) ? !logicalValue : logicalValue
 	}
-	public var logicalFeedbackValue:Bool?
+
+	
+	public var ioFeedbackValue:Bool?
+	public var logicalFeedbackValue:Bool?{
+		guard ioFeedbackValue != nil else{ return nil}
+		return (outputLogic == .inverse) ? !ioFeedbackValue! : ioFeedbackValue!
+	}
 	
 	public init(channelNumber:Int, logic:digitalOutputLogic = .straight) {
 		super.init(channelType: IOType.digitalOut, channelNumber: channelNumber)
 		outputLogic = logic
 	}
 	
-	public var ioValue:Bool = false
 	
-	public var ioFeedbackValue:Bool?{
-		didSet{
-			guard ioFeedbackValue != nil else{ logicalFeedbackValue = nil; return }
-			
-			logicalFeedbackValue = (outputLogic == .inverse) ? !ioFeedbackValue! : ioFeedbackValue!
-		}
-	}
 	
 }
 

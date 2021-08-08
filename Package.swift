e3bc7df7cb12ad6f673251e53c6dac7b06efa728
Jan Verrept <janverrept@me.com>
2021-08-08 21:31:41 +0200

    added JVCocoa as a dependency

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "ClibModbus", url: "https://github.com/TheMisfit68/ClibModbus.git", .branch("master")),
+		.package(name: "JVCocoa", url: "https://github.com/TheMisfit68/JVCocoa.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
